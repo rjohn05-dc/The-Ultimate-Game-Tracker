2a82cde76f816c19a35797062eb27728ee715807
rjohn05-dc <rohanmathewjohn@gmail.com>
2025-04-09 18:21:07 -0700

    started this

---
+++ gameTracker.js
@@ -0,0 +1,26 @@
+
+let games=[
+    [
+        "GOW:RAGNAROK",
+        "Black Myth Wukong",
+        "Spider-Man 2",
+        "Resident Evil 4",
+        "Assasins Creed: Shadows",
+        "Sifu",
+        "Hogwarts Legacy",
+        "Star Wars Jedi: Survivor",
+        "Poppy Playtime",
+        "Street Fighter 6",
+        "Poppy Playtime 4",
+
+    ]
+]
+
+function randomGame(){
+
+}
+
+let nameMap= new Map();
+nameMap.set("Charlie",randomGame());
+nameMap.set("Tom",randomGame());
+nameMap.set("Jerry",randomGame());
